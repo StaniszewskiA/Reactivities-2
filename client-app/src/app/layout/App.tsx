0d00d260ea0d5a1df9941e641cc8311082100f07
AStaniszewski <staniszewski01adam@gmail.com>
2023-03-05 15:52:20 +0100

    Changes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import { Container, Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -22,13 +23,7 @@ function App() {
     <>
         <NavBar/>
         <Container style={{marginTop: '7em'}}>
-          <List>
-            {activities.map(activity => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-          </List>
+          <ActivityDashboard activities={activities}/>
         </Container>
       </>
   );
