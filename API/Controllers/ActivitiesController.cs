dec6f24d5442882f63d1e9ccf9ec166a20dc4d2a
AStaniszewski <staniszewski01adam@gmail.com>
2023-03-14 12:41:40 +0100

    Error handling 3

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers
         [HttpGet] //api/activites
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")] //api/activties/someDetails
@@ -21,7 +21,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+            return HandleResult(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
         [HttpPut("{id}")]
